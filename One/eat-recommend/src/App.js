f5317298f7baec066e3b9ed7042a6f6c2412837b
이정현 <5337myung@gmail.com>
2022-04-07 23:26:09 +0900

    임시경로 json 설정 완료

--- One/eat-recommend/src/App.js
+++ One/eat-recommend/src/App.js
@@ -1,7 +1,13 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 import { FiArrowDown, FiArrowRightCircle } from 'react-icons/fi';
 import {Modal, Button} from "react-bootstrap";
+import { Navbar, Container, Nav} from 'react-bootstrap';
+import axios from "axios";
+import Foodjson from '/Users/ijeonghyeon/github/eat/One/eat-recommend/src/foood.json';
+console.log(Foodjson);
+document.write(JSON.stringify(Foodjson));
+
 
 function App() {
 
@@ -10,10 +16,20 @@ function App() {
 
     return (
      <div className="App">
-         <div className="blank-nav">
-             <button className="box" onClick={ ()=>{ camera변경(!camera)} }>카메라 </button>
-             <button className="box" onClick={ ()=>{ refri변경(!refri)} }>음식 추천</button>
-         </div>
+         <Navbar bg="light" expand="lg">
+             <Container>
+                 <Navbar.Brand href="#home">이현이민</Navbar.Brand>
+                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                 <Navbar.Collapse id="basic-navbar-nav">
+                     <Nav className="me-auto">
+                         {/* Nav.Link 안에 link태그를 작성하게 되면 콘소창에 warning이 뜨게됨으로 Link태그를 삭제해주고 as={Link}를 작성해주어 link태그를 집어넣은 것처처럼 해준다 */}
+                         <button className="box" onClick={ ()=>{ camera변경(!camera)} }>카메라 </button>
+                         <button className="box" onClick={ ()=>{ refri변경(!refri)} }>음식 추천</button>
+                     </Nav>
+                 </Navbar.Collapse>
+             </Container>
+         </Navbar>
+
          <div>
              {
              refri === true
@@ -86,18 +102,29 @@ function Refri() {
     let [pop, pop변경] = useState(false);
     let [modal, modal변경] = useState(false);
 
+    let [food, food변경] = useState(true);
+
+
+    useEffect((Foodjson)=> {
+        axios.get('/Users/ijeonghyeon/github/eat/One/eat-recommend/src/foood.json')
+            .then((result)=>{
+                console.log(result.data)
+                food변경([...food, ...result.data] );
+            })
+
+    })
+
     return (
 
         <div className="refri">
             <div className="container1">
                 <RefriModal show={modal} onHide={()=>{modal변경(false)}} />
                 <div className="item" onClick={()=>{modal변경(true)}}>
-                    <ul>
-                        <li>A-1</li>
-                        <li>A-2</li>
-                        <li>A-3</li>
-                        <li>A-4</li>
-                    </ul>
+                    {
+                        food.map((a,i)=>{ // a는 map이라는 반복문이 돌때마다 들어가는 shoes 데이터, i는 반복문이 돌때마다 들어가는 번호
+                            return <Food food={food[i]} i={i} />
+                        })
+                    }
                 </div>
                 <div className="item" onClick={()=>{modal변경(true)}}>
                     <ul>
@@ -205,7 +232,9 @@ function RefriModal({show, onHide}) {
                 <h4>재료</h4>
                 <p>
                     <ul>
-                        <li>A-1</li>
+                        <li>
+                            <label><input type="checkbox" /> A-1 </label>
+                        </li>
                         <li>A-2</li>
                         <li>A-3</li>
                         <li>A-4</li>
@@ -219,7 +248,13 @@ function RefriModal({show, onHide}) {
     );
 }
 
-
+function Food(props) { // props안에 shoes가 들어가게된다
+    return(
+        <div> {/* 영역안에서 3등분을 하게끔 */}
+            <li><h4>{ props.food.title }</h4></li>
+        </div>
+    )
+}
 
 
 
